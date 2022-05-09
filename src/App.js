37aa9d2aede7c4a6ebbbcedfd4fc6552cc764f48
Nafis Sazid <nafissazid48@gmail.com>
2022-05-09 10:59:58 +0600

    added simple functionalities in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
 
 function App() {
+  const name = 'Brad'
+  const x = false
+  const [tasks, setTasks] = useState([
+    {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+    },
+    {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "Feb 6th at 1:30pm",
+        "reminder": true
+    }
+])
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <Header/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
+    
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +47,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <h1>Hello from React</h1>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2> */}
     </div>
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
